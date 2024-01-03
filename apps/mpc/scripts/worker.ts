65e5952a7ecb0651c6bae716b79b5199a4246923
eabz <eabz@kindynos.mx>
2024-01-03 10:21:21 -0600

    Reorg

--- apps/mpc/scripts/worker.ts
+++ apps/mpc/scripts/worker.ts
@@ -1,16 +1,16 @@
 import { readFileSync, writeFileSync, readdirSync } from 'fs'
 
-const files = readdirSync('./apps/mpc/dist/assets/')
+const files = readdirSync('./dist/assets/')
 
 const workerFile = files.find((files) => files.split('-')[0] === 'worker')
 
 if (workerFile) {
-  const worker = readFileSync(`./apps/mpc/dist/assets/${workerFile}`)
+  const worker = readFileSync(`./dist/assets/${workerFile}`)
 
   // @ts-ignore
   const encoded = Buffer.from(worker, 'binary').toString('base64')
 
   const file = 'export default `' + encoded + '`'
 
-  writeFileSync('./apps/mpc/src/mpc/worker/built.ts', file)
+  writeFileSync('./src/mpc/worker/built.ts', file)
 }
