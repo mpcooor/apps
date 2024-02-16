23becb9b1f5bdde3c5787ab98fcac7f36ad25e79
0xngmi <0xngmi@protonmail.com>
2024-02-16 02:23:32 +0000

    change storage keys

--- packages/types/src/storage.ts
+++ packages/types/src/storage.ts
@@ -13,9 +13,9 @@ export interface IStorageMetadata {
 
 export enum StorageKeys {
   STORAGE_METADATA = 'llama_wallet_storage_metadata',
-  STORAGE_KEYS = 'llama_wallet:storage_keys:',
-  STORAGE_PRESIGNS = 'llama_wallet:storage_presigns:',
-  STORAGE_KEYS_LIST = 'llama_wallet:storage_keys_list',
-  STORAGE_KEYS_DELETED_LIST = 'llama_wallet:storage_keys_deleted_list',
-  STORAGE_PRESIGN_LIST = 'llama_wallet:storage_presigns_list'
+  STORAGE_KEYS = 'llama_wallet.storage_keys.',
+  STORAGE_PRESIGNS = 'llama_wallet.storage_presigns.',
+  STORAGE_KEYS_LIST = 'llama_wallet.storage_keys_list',
+  STORAGE_KEYS_DELETED_LIST = 'llama_wallet.storage_keys_deleted_list',
+  STORAGE_PRESIGN_LIST = 'llama_wallet.storage_presigns_list'
 }
